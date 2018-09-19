b1f3f3bd7f1ad4deb069c65a5861272cec4646c2
djconnolly27 <djconnolly27@gmail.com>
2018-09-18 22:29:24 -0400

    Adds completed adder

--- adder.v
+++ adder.v
@@ -1,11 +1,17 @@
-// Adder circuit
+// Adder circuita
+`define NOT not #50
+`define AND and #50
+`define OR or #50
+`define NAND nand #50
+`define NOR nor #50
+`define XOR xor #50
 
 module behavioralFullAdder
 (
-    output sum, 
+    output sum,
     output carryout,
-    input a, 
-    input b, 
+    input a,
+    input b,
     input carryin
 );
     // Uses concatenation operator and built-in '+'
@@ -14,11 +20,20 @@ endmodule
 
 module structuralFullAdder
 (
-    output sum, 
+    output sum,
     output carryout,
-    input a, 
-    input b, 
+    input a,
+    input b,
     input carryin
 );
+
+    wire AandB, AandCin, BandCin, AxorB;
+
+    `AND andgateAB(AandB, a, b);
+    `AND andgateACin(AandCin, a, carryin);
+    `AND andgateBCin(BandCin, b, carryin);
+    `OR orgateAllAnds(carryout, AandB, AandCin, BandCin);
+    `XOR xorAB(AxorB, a, b);
+    `XOR xorAll(sum, AxorB, carryin);
     // Your adder code here
 endmodule
